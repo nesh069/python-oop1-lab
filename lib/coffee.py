2b4355a36a1b7df009a92f0565a645ffd273ed51
Emmanuel Munene <emmanuel.munene@student.moringaschool.com>
2026-06-06 17:47:57 +0300

    feat: add Coffee class with size property validation and tip method

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if value not in ["small", "Medium", "Large"]:
+            print("size must be Small, Medium, or large.")
+        else:
+            self._size = value
+            
+    def tip(self):
+        print("This coffee is great, here's a tip!")
+        self.price += 1
