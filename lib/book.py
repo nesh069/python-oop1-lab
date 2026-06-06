004360b803f6d6006668cb6385461cda2a95a726
Emmanuel Munene <emmanuel.munene@student.moringaschool.com>
2026-06-06 18:04:31 +0300

    fix: correct Book and Coffee class implementations

--- lib/book.py
+++ lib/book.py
@@ -1,22 +1,18 @@
-#!/usr/bin/env python3
-
 class Book:
-    def __init__(seslf, title, page_count):
+    def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
-        
+
     @property
     def page_count(self):
         return self._page_count
-    
+
     @page_count.setter
     def page_count(self, value):
         if not isinstance(value, int):
-            print("Page count must be an integer.")
+            print("page_count must be an integer")
         else:
             self._page_count = value
-            
+
     def turn_page(self):
-        print("Flipping the page...wow, you read fast!")    
-    
-        
+        print("Flipping the page...wow, you read fast!")
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,21 +1,19 @@
-#!/usr/bin/env python3
-
 class Coffee:
-    def __init__(self, name, price):
-        self.name = name
+    def __init__(self, size, price):
+        self.size = size
         self.price = price
-        
+
     @property
     def size(self):
         return self._size
-    
+
     @size.setter
     def size(self, value):
-        if value not in ["small", "Medium", "Large"]:
-            print("size must be Small, Medium, or large.")
+        if value not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large")
         else:
             self._size = value
-            
+
     def tip(self):
         print("This coffee is great, here's a tip!")
         self.price += 1
